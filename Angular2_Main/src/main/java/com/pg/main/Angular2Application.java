a795a8024d1357a021ddc7bdb69602d8fb3e5bff
Vikas P <Vikas P@DESKTOP-FAC0KPL>
2017-11-18 12:39:06 +0530

    Added Test Projests

---
+++ Angular2_Main/src/main/java/com/pg/main/Angular2Application.java
@@ -0,0 +1,12 @@
+package com.pg.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Angular2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Angular2Application.class, args);
+	}
+}
---
+++ Angular2_Main/src/test/java/com/pg/main/Angular2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pg.main;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Angular2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
