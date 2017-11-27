8432c736846e9c6226b996a4084d8d49eab91453
Vikas P <Vikas P@DESKTOP-FAC0KPL>
2017-11-27 11:08:49 +0530

    Added 'frontend'

--- Angular2_Main/src/main/java/com/pg/main/Angular2Application.java
+++ Angular2_Main/src/main/java/com/pg/main/Angular2Application.java
@@ -2,8 +2,10 @@ package com.pg.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.pg.contrlr"})
 public class Angular2Application {
 
 	public static void main(String[] args) {
